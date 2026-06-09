6057105168078e02824fcf487e9f478273bc96c2
AlbeeSo <suyashi1321@163.com>
2026-06-09 17:30:29 +0800

    feat: support mount-proxy socket flags and skipGlobalMount env for sandbox scenarios
    Add --mount-proxy-sock and --customfuse-mount-proxy-sock flags to configure mount-proxy socket paths for sandbox agent scenarios.
    Support SKIP_GLOBAL_MOUNT env to skip customfuse globalMount in csi-plugin.

--- pkg/oss/utils.go
+++ pkg/oss/utils.go
@@ -823,6 +823,7 @@ func getDirectAssignedValue(runtimeClass string) bool {
 	}
 }
 
+
 func needRotateToken(fuseType string, secrets map[string]string) (needRotate bool) {
 	if len(secrets) == 0 {
 		return false
